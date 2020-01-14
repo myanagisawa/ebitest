5b14d530f8d54e6109415581b185555de9d48605
myanagisawa <myanagisawa@beerholic.jp>
2020-01-14 11:59:38 +0900

    ゆーたbomb画像に円形マスクを適用

--- ebitest/game.go
+++ ebitest/game.go
@@ -2,10 +2,15 @@ package ebitest
 
 import (
 	"fmt"
+	"image"
+	"os"
 	"sync"
 
+	"log"
+
+	"golang.org/x/image/draw"
+
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 const (
@@ -27,19 +32,37 @@ type (
 func NewGame(paths, objPaths []string) (*Game, error) {
 	fmt.Print("loading images")
 	images := make([]ebiten.Image, len(objPaths))
+	// mask画像読み込み
+	mask, _ := getImageByPath("system_images/mask.png")
 
 	wg := &sync.WaitGroup{}
 	for i, path := range objPaths {
 		wg.Add(1)
-		go func(list []ebiten.Image, idx int, path string) {
-			img, _, err := ebitenutil.NewImageFromFile(path, ebiten.FilterDefault)
+		go func(list []ebiten.Image, idx int, path string, maskimg image.Image) {
+			// 画像読み込み
+			img, _ := getImageByPath(path)
+			// 画像サイズに合わせたマスクの作成
+			rmask := image.NewRGBA(img.Bounds())
+			draw.BiLinear.Scale(rmask, rmask.Bounds(), maskimg, maskimg.Bounds(), draw.Over, nil)
+			// 円形maskの適用
+			// log.Printf("img.bounds: %#v", img.Bounds())
+			// log.Printf("mask.bounds: %#v", rmask.Bounds())
+			out := image.NewRGBA(img.Bounds())
+			draw.DrawMask(out, out.Bounds(), img, image.Point{0, 0}, rmask, image.Point{0, 0}, draw.Over)
+
+			// 画像からebiten.imageを作成
+			// img, _, err := ebitenutil.NewImageFromFile(path, ebiten.FilterDefault)
+			// if err != nil {
+			// 	panic(err)
+			// }
+			ebimg, err := ebiten.NewImageFromImage(out, ebiten.FilterDefault)
 			if err != nil {
 				panic(err)
 			}
-			list[idx] = *img
+			list[idx] = *ebimg
 			fmt.Print(".")
 			wg.Done()
-		}(images, i, path)
+		}(images, i, path, mask)
 	}
 	wg.Wait()
 	fmt.Println("complete!")
@@ -68,4 +91,22 @@ func (g *Game) Update(r *ebiten.Image) error {
 	g.sceneManager.Draw(r)
 
 	return nil
+}
+
+func getImageByPath(path string) (image.Image, string) {
+	//画像読み込み
+	fileIn, err := os.Open(path)
+	defer fileIn.Close()
+	if err != nil {
+		fmt.Println("error:file\n", err)
+		log.Panic(err.Error())
+	}
+
+	//画像をimage型として読み込む
+	img, format, err := image.Decode(fileIn)
+	if err != nil {
+		fmt.Println("error:decode\n", format, err)
+		log.Panic(err.Error())
+	}
+	return img, format
 }
